4126996e9b46662b96999fe1ef80c04d98161d43
martin frances <martinfrances107@hotmail.com>
2020-08-25 10:12:58 +0100

    I don't need valueOf, so removing skeleton implemnation as it is buggy. Also simplifed working code to remove option

--- src/extent.rs
+++ src/extent.rs
@@ -1,76 +1,35 @@
 use std::cmp::PartialOrd;
 
-pub fn extent<T>(
-  values: Vec<Option<T>>,
-  valueof: Option<Box<dyn Fn(Option<T>) -> Option<T>>>,
-) -> [Option<T>; 2]
-where T: PartialOrd + Copy{
+pub fn extent<T>(values: Vec<T>) -> [T; 2]
+where
+  T: PartialOrd + Copy,
+{
   let mut min: Option<T> = None;
   let mut max: Option<T> = None;
-  match valueof {
-    None => {
-      for value in values {
-        match value {
-          Some(value_val) => {
-            match min {
-              None => {
-                min = value;
-              }
-              Some(min_val) => {
-                if min_val > value_val {
-                  min = value;
-                }
-              }
-            }
-            match max {
-              None => {
-                max = value;
-              }
-              Some(max_val) => {
-                if max_val < value_val {
-                  max = value
-                }
-              }
-            }
-          }
-          None => {}
+  for value in values {
+    match min {
+      None => {
+        min = Some(value);
+      }
+      Some(min_val) => {
+        if min_val > value {
+          min = Some(value);
         }
       }
     }
-
-    Some(valueof) => {
-      // let _index = -1;
-      for value in values {
-        let value_of_out = valueof(value);
-        match value_of_out {
-          Some(value_of_out) => {
-            match min {
-              Some(min_val) => {
-                if min_val > value_of_out {
-                  min = Some(value_of_out);
-                }
-              }
-              None => {
-                min = Some(value_of_out);
-              }
-            }
-            match max {
-              Some(max_val) => {
-                if max_val < value_of_out {
-                  max = Some(value_of_out);
-                }
-              }
-              None => {
-                max = Some(value_of_out);
-              }
-            }
-          }
-          None => {}
+    match max {
+      None => {
+        max = Some(value);
+      }
+      Some(max_val) => {
+        if max_val < value {
+          max = Some(value);
         }
       }
     }
   }
-  return [min, max];
+
+  return [min.unwrap(), max.unwrap()];
 }
 
 // export default function(values, valueof) {
