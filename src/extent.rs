3f26f0632565600cfa792b05dc4325d888624767
Martin <martinfrances107@hotmail.com>
2020-11-05 19:21:22 +0000

    ran cargo fmt

--- src/extent.rs
+++ src/extent.rs
@@ -2,34 +2,34 @@ use std::cmp::PartialOrd;
 
 pub fn extent<T>(values: Vec<T>) -> [T; 2]
 where
-  T: PartialOrd + Copy,
+    T: PartialOrd + Copy,
 {
-  let mut min: Option<T> = None;
-  let mut max: Option<T> = None;
-  for value in values {
-    match min {
-      None => {
-        min = Some(value);
-      }
-      Some(min_val) => {
-        if min_val > value {
-          min = Some(value);
+    let mut min: Option<T> = None;
+    let mut max: Option<T> = None;
+    for value in values {
+        match min {
+            None => {
+                min = Some(value);
+            }
+            Some(min_val) => {
+                if min_val > value {
+                    min = Some(value);
+                }
+            }
         }
-      }
-    }
-    match max {
-      None => {
-        max = Some(value);
-      }
-      Some(max_val) => {
-        if max_val < value {
-          max = Some(value);
+        match max {
+            None => {
+                max = Some(value);
+            }
+            Some(max_val) => {
+                if max_val < value {
+                    max = Some(value);
+                }
+            }
         }
-      }
     }
-  }
 
-  return [min.unwrap(), max.unwrap()];
+    return [min.unwrap(), max.unwrap()];
 }
 
 // export default function(values, valueof) {
