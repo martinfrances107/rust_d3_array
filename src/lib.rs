4d4ad830d15cd2c2d7feb672db293745c119f251
Martin <martinfrances107@hotmail.com>
2021-10-28 07:29:26 +0100

    Added crate level documentation and description like fields in Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
 #![allow(clippy::pedantic)]
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
+//! This crate contains helper functions for array manipulation, ordering, searching, summarizing, etc.
+//!
+//! <hr>
+//!
+//! Repository [rust_d3_array](<https://github.com/martinfrances107/rust_d3_array>)
+//!
+//! A port of [d3/d3-array](<https://github.com/d3/d3-array>)
 
 extern crate num_traits;
 
+/// Extent is the min and max values of a collection.
 pub mod extent;
+/// Helper functions for flatterning a collection.
 pub mod merge;
+/// Helper functions for generating a collection.
 pub mod range;
